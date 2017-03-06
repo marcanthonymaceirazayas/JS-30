896033d0d9b297fb689d3770d35e43d76911bf9d
Marc Anthony Maceira Zayas <mmaceira.zayas@gmail.com>
2017-03-06 00:48:24 -0400

    Day 2: CSS + JS Clock

---
+++ 02-CSS+JS-Clock/Clock.js
@@ -0,0 +1,24 @@
+
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+function setDate(){
+  const now = new Date();
+  
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  console.log('Second: ', seconds);
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  console.log('Minute: ', minutes);
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 60) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  console.log('Hour: ', hours);
+}
+
+setInterval(setDate, 1000);
