b4750f15a4dfef7706fd9ee49a4775c6911b03a5
Marc Anthony Maceira Zayas <mmaceira.zayas@gmail.com>
2017-03-13 23:39:22 -0400

    Fix
    Fixed hours counting down as minutes.

--- 02-CSS+JS-Clock/Clock.js
+++ 02-CSS+JS-Clock/Clock.js
@@ -4,7 +4,7 @@ const minuteHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
 function setDate(){
   const now = new Date();
-  
+
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
@@ -16,7 +16,7 @@ function setDate(){
   console.log('Minute: ', minutes);
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 60) * 360) + 90;
+  const hoursDegrees = ((hours / 24) * 360) + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
   console.log('Hour: ', hours);
 }
